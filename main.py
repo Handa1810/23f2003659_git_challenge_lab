aa9e8475d73ab6fc1b2b16c8d302954ada7b9075
Handa1810 <hyash2455@gmail.com>
2025-09-15 23:03:43 +0530

    Add main.py as project entry point

---
+++ main.py
@@ -0,0 +1,8 @@
+from sum import add
+from difference import subtract
+from product import multiply
+
+if __name__ == "__main__":
+    print("5 + 3 =", add(5, 3))
+    print("5 - 3 =", subtract(5, 3))
+    print("5 * 3 =", multiply(5, 3))
